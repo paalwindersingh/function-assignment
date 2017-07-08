d4ae2a5a4f69dd39a84035543975241df111957c
paalwinder <jkaur@twg.io>
2017-07-08 22:29:32 +0530

    add functions

--- function-assignment.cs
+++ function-assignment.cs
@@ -1,11 +1,108 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace FunctionAssignment
 {
    class FunctionAssignment
    {
-      static void Main(string[] args)
-      {
-         Console.Write("Test");
-      }
+        static void Main(string[] args)
+        {
+            Console.Write("enter value of a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter value of b ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter value of c ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(high(a, b, c));
+            Console.WriteLine(high(a, b));
+            Console.WriteLine(low(a, b, c));
+            Console.WriteLine(low(a, b));
+            Console.WriteLine(isOdd(a));
+            Console.WriteLine(isEven(b));
+            Console.ReadKey();
+        }
+
+        static int high(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+
+            }
+            else
+            {
+                return b;
+            }
+        }
+        static int low(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        static int high(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+
+            }
+            if (b > a && b > c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+
+        static int low(int a, int b, int c)
+        {
+            if (a < b && a < c)
+            {
+                return a;
+
+            }
+            if (b < a && b < c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+
+        static bool isOdd(int a)
+        {
+            if (a % 2 == 1)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        static bool isEven(int b)
+        {
+            if (b % 2 == 0)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
    }
 }
